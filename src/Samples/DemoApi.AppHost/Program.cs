76494879504ef1bf8c1f3199dd64160e7c4217f8
Alejandro <acksecurity@hotmail.com>
2024-04-26 14:40:23 -0400

    Fix aspire dashboard

--- src/Samples/DemoApi.AppHost/Program.cs
+++ src/Samples/DemoApi.AppHost/Program.cs
@@ -1,5 +1,5 @@
 var builder = DistributedApplication.CreateBuilder(args);
 
-builder.AddProject<Projects.DemoApi_Postgresql>("demoapi.postgresql");
+builder.AddProject<Projects.DemoApi_Postgresql>("demoapi-postgresql");
 
 builder.Build().Run();
--- src/Samples/DemoApi.Postgresql/Program.cs
+++ src/Samples/DemoApi.Postgresql/Program.cs
@@ -7,6 +7,7 @@ var builder = WebApplication.CreateBuilder(args);
 
 builder.AddServiceDefaults();
 
+
 // Add services to the container.
 builder.Services.AddScoped<ITodoService, TodoService>();
 
